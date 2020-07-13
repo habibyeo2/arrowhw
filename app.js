55f8c2cc221ba041e530fc28548a19bc5aa789fc
Habib Yeo <habibyeo2@gmail.com>
2020-07-13 00:24:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var a = 1;
+var b = 2;
+
+function uno( arguments1, arguments2){
+    return  arguments1 +  arguments2;
+}
+console.log(uno(a,b))
+
+
+function dos( arguments1, arguments2){
+    return  arguments1 -  arguments2;
+
+}
+console.log(dos(a,b))
+
+function tres( arguments1, arguments2){
+    return  arguments1 *  arguments2;
+}
+console.log(tres(a,b))
+
+function forgot( arguments1, arguments2){
+    return  arguments1 /  arguments2;
+}
+console.log(forgot(a,b))
+
+
+const uno = (arguments1,arguments2) => arguments1+ arguments2;
+
+
+const dos = (arguments3,arguments4) => arguments3 - arguments4;
+
+
+const tres = (arguments5,arguments6) => arguments5 * arguments6;
+
+
+const forgot = (arguments7,arguments8) => arguments8 / arguments7;
+
